aaff4776473588736998c19b8902c0b8c02c2430
mtzcpd1847 <mtzcpd1847>
2020-02-21 16:27:51 -0300

    ajustando imagem

--- latest/docker-entrypoint.sh
+++ latest/docker-entrypoint.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-set -e
-SCREEN="${SCREEN:-1280x1024x16}"
-echo "Starting X virtual framebuffer (Xvfb) for $SCREEN screen in background..."
-Xvfb -ac :99 -screen 0 $SCREEN > /dev/null 2>&1 &
-export DISPLAY=:99
+# set -e
+# SCREEN="${SCREEN:-1280x1024x16}"
+# echo "Starting X virtual framebuffer (Xvfb) for $SCREEN screen in background..."
+# Xvfb -ac :99 -screen 0 $SCREEN > /dev/null 2>&1 &
+# export DISPLAY=:99
 
-exec "$@"
+# exec "$@"
+
+echo "http://dl-4.alpinelinux.org/alpine/v3.9/main/" >> /etc/apk/repositories && \
+echo "http://dl-4.alpinelinux.org/alpine/v3.9/community/" >> /etc/apk/repositories
+
+
+apk update && \
+  apk add build-base \
+  libxml2-dev \
+  libxslt-dev \
+  curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
+  && rm -rf /var/cache/apk/*
